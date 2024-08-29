9c06006f38b3bd4b534fbb1c62cae5080825481c
Gowthami Shyam <158075220+GowthamiShyam15@users.noreply.github.com>
2024-08-29 15:51:09 +0530

    Update AppDownload.jsx

--- Frontend/src/components/AppDownload/AppDownload.jsx
+++ Frontend/src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-        <p>For Better Experience DOWNLOAD <br /> Tomato App</p>
+        <p>For Better Experience DOWNLOAD <br />Jomato App</p>
         <div className="app-downlaod-platforms">
             <img src={assets.play_store} alt="" />
             <img src={assets.app_store} alt="" />
